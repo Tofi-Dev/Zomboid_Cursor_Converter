76e8ee5a89408602e2a2db37b81fb5a55e6049c4
Tofi-Dev <blazejmaly1@gmail.com>
2024-06-11 15:37:35 +0200

    Update main.py
    Fixed grammar issues, removed the bold font and added some error checking.

--- main.py
+++ main.py
@@ -22,36 +22,44 @@
 
 import cv2
 import FreeSimpleGUI as sg
-
+import sys
 
 sg.theme('Reddit')
 
+version = [0,1,0,"b"]
+#Format of version: [major, minor, patch, revision]
+
 credits = """Developed by TofiToffee 2024. Based on the MIT license.
 
-This is not affiliated with The Indie Stone."""
+I'm not affliated with The Indie Stone.
+
+""" + "Current version: " + ".".join(str(x) for x in version)
 
 help = """Due to a Bug in the game's engine, the cursor renders with Red and Blue Channels flipped 
 I've developed this tool to provide a temporary fix.
 
 How to use:
 
-1. Select an image to convert ( preferably a image that is 32x32 ).
+1. Select an image to convert (32x32 preferably).
 2. Select an output name.
-3. Press Convert.
+3. Press convert.
 4. The cursor will be saved in the same directory as the image.
-5. Go to Steam and go Project Zomboid
-6. Open the local folder and go to media/ui
+5. Go to Steam and go to Project Zomboid.
+6. Open the local folder and go to media/ui.
 7. Replace the cursor_white.png with your custom one."""
 
 layout = [[sg.Text('Select image')],
-              [sg.Input(), sg.FileBrowse(file_types=(('PNG Files', '*.png'),))],
-              [sg.Text('Output name')],
-              [sg.Input("cursor_white.png")],
-              [sg.Button('Convert'), sg.Button('Exit'), sg.Button('About'), sg.Button('Help')]]
+            [sg.Input(), sg.FileBrowse(file_types=(('PNG Files', '*.png'),))],
+            [sg.Text('Output name')],
+            [sg.Input("cursor_white.png")],
+            [sg.Button('Convert'), sg.Button('Exit'), sg.Button('About'), sg.Button('Help')]]
 
-window = sg.Window('Project Zomboid - Cursor Converter', layout, icon="icon.ico", font=("Tahoma",12, "bold"))
+window = sg.Window('Project Zomboid - Cursor Converter ' + "v" + ".".join(str(x) for x in version), layout, icon="icon.ico", font=("Tahoma",12))
 
 def convert_image(img,final_name):
+    if img == "":
+        sg.popup('Please select an image', title="Error", font=("Tahoma",12))
+        return
     #Import the image
     img1 = cv2.imread(str(img), cv2.IMREAD_UNCHANGED)
 
@@ -60,6 +68,8 @@ def convert_image(img,final_name):
 
     #Save the image acording to the name
     cv2.imwrite(str(final_name), img2)
+    sg.popup('Conversion complete', title="Success", font=("Tahoma",12))
+
 
 while True:
     event, values = window.read()
@@ -71,12 +81,10 @@ while True:
     #Check for About
     if event == 'About':
         #Popup with credits
-        sg.popup(credits, title="About",font=("Tahoma",12 , "bold"))
+        sg.popup(credits, title="About",font=("Tahoma",12 ))
 
     #Check for Convert
     if event == 'Convert':
         convert_image(values[0],values[1])
-        sg.popup('Conversion complete')
-
     if event == 'Help':
-        sg.popup(help, title="About",font=("Tahoma",12, "bold"))
+        sg.popup(help, title="About",font=("Tahoma",12))
